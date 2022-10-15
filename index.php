d6465992da49dcf32caac7824351c06e9ce2d740
Junior Diedhiou <76671685+Juniorito90@users.noreply.github.com>
2022-10-15 20:58:57 +0000

    Ajout Images

--- index.php
+++ index.php
@@ -1,47 +1,13 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="css/index.css">
-    </head>
-    <body>
-        <div class="v17_44">
-            <div class="v17_90"></div>
-            <div class="v17_91"></div>
-            <div class="v17_66"></div>
-            <div class="v17_92"></div>
-            <span class="v17_93">
-                Connectez-vous à votre compte GoMyCode
-                 Learn pour continuer votre expérience d'apprentissage.
-            </span>
-            <div class="v17_95">
-                <div class="v17_45"></div>
-                <span class="v17_69">CONNEXION</span>
-                <div class="v17_70"></div>
-                <span class="v17_75">Entrer votre email........</span>
-                <span class="v17_76">Entrer votre password........</span>
-                <div class="v17_71"></div>
-                <div class="v17_72"></div>
-                <span class="v17_89">Connexion</span>
-                <span class="v17_74">Email *</span>
-                <span class="v17_77">Passwords *</span>
-                <div class="v17_79"><div class="v17_80"></div>
-                <div class="v17_81"><div class="v17_82"></div>
-            </div>
-            <div class="v17_83">
-                <div class="v17_84"></div>
-            </div>
-        </div>
-        <div class="v17_85">
-            <div class="v17_86"></div>
-            <div class="v17_87"></div>
-            <div class="v17_88"></div>
-        </div>
-        <span class="v17_94">Mot de passe oublié?</span>
-    </div>
-    <div class="v17_96">
-        <div class="v17_97"></div>
-    </div>
-</div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOMYCODE - Connexion</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    
 </body>
 </html>
