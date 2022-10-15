c72a6a4eed43cb898b140dfa403e5a80d562b3c9
Lamine <alaminend23@gmail.com>
2022-10-15 20:56:05 +0000

    Ajout Inscription

---
+++ inscription.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="v13_2">
+        <div class="v13_3"></div>
+        <div class="v13_4"></div>
+        <div class="v13_5"></div>
+        <div class="v13_14"></div>
+        <div class="v13_16"></div>
+        <div class="v16_8">
+            <div class="v16_9"></div>
+            <div class="v16_10">
+                <div class="v16_11"></div>
+            </div>
+            <div class="v16_12">
+                <div class="v16_13"></div>
+            </div>
+        </div>
+        <div class="v13_18"></div>
+        <div class="v16_71"></div>
+        <div class="v16_77"></div>
+        <div class="v16_73">
+            <div class="v16_74"></div>
+            <div class="v16_75"></div>
+            <div class="v16_76"></div>
+        </div>
+        <div class="v16_78">
+            <div class="v16_79"></div>
+            <div class="v16_80"></div>
+            <div class="v16_81"></div>
+        </div>
+        <div class="v16_14">
+            <div class="v16_15">
+                <div class="v16_16"></div>
+            </div>
+            <div class="v16_17">
+                <div class="v16_18"></div>
+            </div>
+            <div class="v16_19"></div>
+            <div class="v16_20"></div>
+            <div class="v16_21"></div>
+        </div>
+        <div class="v16_4">
+            <div class="v16_5"></div>
+        </div><span class="v13_6">Inscription</span>
+        <div class="v13_11"></div>
+        <div class="v13_10"></div>
+        <div class="v23_52"></div>
+        <span class="v13_12">Nom *</span>
+        <span class="v13_13">Prenom *</span>
+        <span class="v13_15">Numéro de Téléphone*</span>
+        <span class="v13_17">E-Mail*</span>
+        <span class="v13_19">Formation souhaiter*</span>
+        <span class="v16_72">Mot de passe*</span>
+        <span class="v16_82">Confirmation mot de passe</span>
+        <div class="v16_6"></div>
+        <span class="v16_7">Valider</span>
+        <div class="v16_22">
+            <div class="v16_23">
+                <div class="v16_24"></div>
+            </div>
+            <div class="v16_25">
+                <div class="v16_26"></div>
+            </div>
+        </div><span class="v16_29">Copyright © Gomycode.com</span>
+        <div class="v20_84"></div>
+        <div class="v20_85">
+            <div class="v20_86"></div>
+        </div>
+    </div>
+</body>
+
+</html>
