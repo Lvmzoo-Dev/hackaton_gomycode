b4e57dfc7b8c618ccce5a2ce2d7960bca06bbd65
Lamine <alaminend23@gmail.com>
2022-10-15 20:29:25 +0000

    page de connexion

---
+++ connexion.php
@@ -0,0 +1,16 @@
+<?php
+
+function pdo_connect_mysql()
+{
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = '';
+    $db_name = 'gomycode';
+
+    try {
+        $db = new PDO("mysql:host=" . $db_host . ";dbname=" . $db_name, $db_user, $db_password);
+        return $db;
+    } catch (Exception $e) {
+        echo "ERREUR DE CONNEXION A LA BASE DE DONNEES !!";
+    }
+}
