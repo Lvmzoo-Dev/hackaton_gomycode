269f7207db32ec3f19d7774e31c19de3965da11a
Lamine <alaminend23@gmail.com>
2022-10-16 05:25:00 +0000

    Update

--- functions/etudiantFunctions.php
+++ functions/etudiantFunctions.php
@@ -1,9 +1,16 @@
 <?php
-
+require_once 'connexion.php';
 function addEtu($nom, $prenom, $email, $password, $formation)
 {
     global $db;
     $sql = "INSERT INTO etudiant 
             VALUES (NULL,'$nom','$prenom','$email','$password','$formation' )";
     $req = mysqli_query($db, $sql);
+}
+function listerEtu()
+{
+    global $db;
+    $sql = "SELECT * FROM etudiant";
+    $req = mysqli_query($db, $sql);
+    return $req;
 }
