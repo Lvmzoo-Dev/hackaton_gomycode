5017c00097d069933b0a306769256d7099a539bd
Junior Diedhiou <76671685+Juniorito90@users.noreply.github.com>
2022-10-16 12:03:06 +0000

    Modification Dashboard Prof 1

--- dashboardProf.php
+++ dashboardProf.php
@@ -20,27 +20,14 @@
 
       <a href="dashboardProf.php" class="logo"><img src="images/logo.png" alt=""></a>
 
-      <form action="search.html" method="post" class="search-form">
+      <form action="" method="post" class="search-form">
          <input type="text" name="search_box" required placeholder="Chercher cours..." maxlength="100">
          <button type="submit" class="fas fa-search"></button>
       </form>
 
       <div class="icons">
-         <div id="menu-btn" class="fas fa-bars"></div>
          <div id="search-btn" class="fas fa-search"></div>
-         <div id="user-btn" class="fas fa-user"></div>
-         <div id="toggle-btn" class="fas fa-sun"></div>
-      </div>
-
-      <div class="profile">
-         <img src="images/pic-1.jpg" class="image" alt="">
-         <h3 class="name">Prof 1</h3>
-         <p class="role">Enseignant</p>
-         <a href="profile.html" class="btn">Voir profile</a>
-         <div class="flex-btn">
-            <a href="login.html" class="option-btn">Connexion</a>
-            <a href="register.html" class="option-btn">Inscription</a>
-         </div>
+         <a href="connexionProf.php"><div id="user-btn" class="fas fa-user"></div></a>
       </div>
 
    </section>
@@ -57,14 +44,13 @@
       <img src="images/pic-1.jpg" class="image" alt="">
       <h3 class="name">Prof 1</h3>
       <p class="role">Enseignant</p>
-      <a href="profile.html" class="btn">Voir profile</a>
+      <a href="#" class="btn">Voir profile</a>
    </div>
 
    <nav class="navbar">
-      <a href="home.html"><i class="fas fa-home"></i><span>Accueil</span></a>
-      <a href="courses.html"><i class="fas fa-graduation-cap"></i><span>cours</span></a>
-      <a href="teachers.html"><i class="fas fa-chalkboard-user"></i><span>Eleves</span></a>
-      <a href="contact.html"><i class="fas fa-headset"></i><span>contact</span></a>
+      <a href="#"><i class="fas fa-home"></i><span>Accueil</span></a>
+      <a href="#"><i class="fas fa-graduation-cap"></i><span>cours</span></a>
+      <a href="#"><i class="fas fa-envelope"></i><span>Messagerie</span></a>
    </nav>
 
 </div>
@@ -73,15 +59,7 @@
 
    <div class="row">
 
-      <div class="image">
-         <img src="images/about-img.svg" alt="">
-      </div>
-
-      <div class="content">
-         <h3>Professeur 1</h3>
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut dolorum quasi illo? Distinctio expedita commodi, nemo a quam error repellendus sint, fugiat quis numquam eum eveniet sequi aspernatur quaerat tenetur.</p>
-         <a href="courses.html" class="inline-btn">Découvrir cours</a>
-      </div>
+      
 
    </div>
 
@@ -89,125 +67,37 @@
 
 <section class="reviews">
 
-   <h1 class="heading">Autres cours</h1>
+   <h1 class="heading">VUE D'ENSEMBLE</h1>
 
    <div class="box-container">
 
       <div class="box">
+         <div class="title1">MES COURS</div>
          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, suscipit a. Quibusdam, dignissimos consectetur. Sed ullam iusto eveniet qui aut quibusdam vero voluptate libero facilis fuga. Eligendi eaque molestiae modi?</p>
          <div class="student">
-            <img src="images/pic-2.jpg" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
-            </div>
+            <button class="voir">Voir</button>
          </div>
       </div>
 
       <div class="box">
+      <div class="title2">NOUVEAUX MESSAGES</div>
          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, suscipit a. Quibusdam, dignissimos consectetur. Sed ullam iusto eveniet qui aut quibusdam vero voluptate libero facilis fuga. Eligendi eaque molestiae modi?</p>
          <div class="student">
-            <img src="images/pic-3.jpg" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
-            </div>
+            <button class="voir">Voir</button>
          </div>
       </div>
 
       <div class="box">
+         <div class="title3">SUPPORT DE COURS </div>
          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, suscipit a. Quibusdam, dignissimos consectetur. Sed ullam iusto eveniet qui aut quibusdam vero voluptate libero facilis fuga. Eligendi eaque molestiae modi?</p>
          <div class="student">
-            <img src="images/pic-4.jpg" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <div class="box">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, suscipit a. Quibusdam, dignissimos consectetur. Sed ullam iusto eveniet qui aut quibusdam vero voluptate libero facilis fuga. Eligendi eaque molestiae modi?</p>
-         <div class="student">
-            <img src="images/pic-5.jpg" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <div class="box">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, suscipit a. Quibusdam, dignissimos consectetur. Sed ullam iusto eveniet qui aut quibusdam vero voluptate libero facilis fuga. Eligendi eaque molestiae modi?</p>
-         <div class="student">
-            <img src="images/pic-6.jpg" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <div class="box">
-         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, suscipit a. Quibusdam, dignissimos consectetur. Sed ullam iusto eveniet qui aut quibusdam vero voluptate libero facilis fuga. Eligendi eaque molestiae modi?</p>
-         <div class="student">
-            <img src="images/pic-7.jpg" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
-            </div>
+            <button class="voir">Voir</button>
          </div>
       </div>
 
    </div>
 
 </section>
-
-
-
-
-
-
-
-
-
-
 
 
 
