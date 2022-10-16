16a9a4a4c77b6f904d4aee79be7a3c60b480b83e
Noudjalta D. Charles-Frédéric <73391620+Charly-programmeur@users.noreply.github.com>
2022-10-16 07:49:04 +0000

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+    <link rel="stylesheet" href="css/accueil.css">
+
+</head>
+
+<body>
+
+    <header>
+
+        <div class="header-1">
+
+            <a href="#" class="logo"><img src="GOMYCODE/LOGO GOMYCODE.png"> </a>
+
+            <div class="header-2">
+
+                <div id="menu" class="fas fa-bars" onclick="navToggle();"></div>
+
+                <nav class="navbar">
+                    <ul>
+                        <li><a onclick="navToggle();" href="#">Nos formations</a></li>
+                        <li><a onclick="navToggle();" href="#">Choisir vos horaire</a></li>
+                        <li><a onclick="navToggle();" href="#">Hackerspace</a></li>
+                        <li><a onclick="navToggle();" href="#">En ligne</a></li>
+                        <li><a onclick="navToggle();" href="#">+221 77 526 38 59</a></li>
+
+                        <li><a class="rouge" href="#">S'inscrire </a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </div>
+
+    </header>
+
+
+    <div class="gauche">
+
+    </div>
+
+    <div class="droite">
+        <h1>Learn Web Coding <br>by Building</h1>
+        <p>Nous formons la prochaine génération tech aux technologies<br> les plus récentes et aux métiers du futur pour les préparer au<br>monde professionnel et renforcer leur employabilité.</p>
+    </div>
+
+
+</body>
+
+</html>
