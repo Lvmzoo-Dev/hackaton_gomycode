77e00c132a2060c130633d8d501539b8b833ae0d
Noudjalta D. Charles-Frédéric <73391620+Charly-programmeur@users.noreply.github.com>
2022-10-16 06:54:56 +0000

    Add files via upload

---
+++ dashboardEtu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOMYCODE - Dashboard Etudiant</title>
+    <link rel="stylesheet" href="css/dashboardEtu.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="div-left">
+            <div class="profil">
+                <div class="user-profil"></div>
+                <div class="user-infos">
+                    <p class="nom">Nom</p>
+                    <p class="prenom">Prenom</p>
+                    <p style="font-weight: bold;">Etudiant</p>
+                </div>
+            </div>
+            <div class="separation"></div>
+            
+            <nav>
+                <ul>
+                    <li><img src="images/home.png" class="icon" alt="">  <a href="#">Accueil</a></li>
+                    <li><img src="images/cours.png" class="icon" alt="">  <a href="#">Cours</a></li>
+                    <li><img src="images/prof.png" class="icon" alt="">  <a href="#">Professeur</a></li>
+                    <li><img src="images/espace-etudiant.png" class="icon" alt="">  <a href="#">Espace Etudiant</a></li>
+                </ul>
+            </nav>
+
+        </div>
+        <div class="div-middle"></div>
+        <div class="div-right"></div>
+    </div>
+
+</body>
+</html>
