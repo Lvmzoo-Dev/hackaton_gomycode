7f6643c15d40da8c3e8c3dc2ad5e3e8f6c7bd502
Lamine <alaminend23@gmail.com>
2022-10-16 04:55:49 +0000

    Update Index

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 
-// @include_once 'connexion.php';
-// pdo_connect_mysql();
+require_once 'functions/etudiantFunctions.php';
+$etudiants = listerEtu();
+
+foreach ($etudiants as $etu) :
+
+    $email = $etu['emailEtu'];
+    $password = $etu['passwordEtu'];
+
+endforeach;
+
+if (isset($_POST['valider'])) {
+    if ($email != $_POST['email'] || $password != $_POST['password']) {
+?>
+        <script>
+            alert("VOUS N'ETES PAS SUR LA BASE DE DONNEES");
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            alert("VOUS ETES BIEN SUR LA BASE DE DONNEES");
+        </script>
+<?php
+    }
+}
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,31 +48,33 @@
 
     <div class="container">
         <img src="images/img2.png" alt="">
-        <div class="form">
-            <p>
-                Connectez-vous à votre compte <span>GoMyCode</span> Learn pour
-                continuer votre expérience d'apprentissage.
-            </p>
-            <nav>
+        <form action="" method="POST">
+            <div class="form">
+                <p>
+                    Connectez-vous à votre compte <span>GoMyCode</span> Learn pour
+                    continuer votre expérience d'apprentissage.
+                </p>
+                <nav>
 
-                <h2>CONNEXION</h2>
-                <div class="content">
+                    <h2>CONNEXION</h2>
+                    <div class="content">
 
-                    <div>
-                        <label for="">Email *</label><br>
-                        <input type="email" name="email" placeholder="Entrer votre email" autocomplete="off">
+                        <div>
+                            <label for="">Email *</label><br>
+                            <input type="email" name="email" placeholder="Entrer votre email" autocomplete="off">
+                        </div>
+                        <div>
+                            <label for="">Password *</label><br>
+                            <input type="password" name="password" placeholder="Entrer votre mot de passe" autocomplete="off">
+                        </div>
+
+                        <button type="submit" name="valider" id="valider" class="btn">CONNEXION</button><br>
+                        <a href="inscription.php" class="forgotpwd">Vous n'avez pas de compte ?</a>
                     </div>
-                    <div>
-                        <label for="">Password *</label><br>
-                        <input type="password" name="email" placeholder="Entrer votre mot de passe" autocomplete="off">
-                    </div>
 
-                    <a class="btn">CONNEXION</a><br>
-                    <a href="#" class="forgotpwd">Mot de passe oublié ?</a>
-                </div>
-
-            </nav>
-        </div>
+                </nav>
+            </div>
+        </form>
     </div>
 
 </body>
