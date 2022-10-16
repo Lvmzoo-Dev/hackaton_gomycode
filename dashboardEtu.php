912acf538e449472462469cec466aaea3d701b5d
Lamine <alaminend23@gmail.com>
2022-10-16 05:50:48 +0000

    gestion Message Prof-Etudiant

--- dashboardEtu.php
+++ dashboardEtu.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +8,9 @@
     <title>GOMYCODE - Dashboard Etudiant</title>
     <link rel="stylesheet" href="css/dashboardEtu.css">
 </head>
+
 <body>
-    
+
     <?php
 
     // @include 'nav.php';
@@ -25,14 +27,14 @@
                 </div>
             </div>
             <div class="separation"></div>
-            
+
             <nav>
                 <ul>
-                    <li><img src="images/home.png" class="icon" alt="">  <a href="#">Accueil</a></li>
-                    <li><img src="images/cours.png" class="icon" alt="">  <a href="#">Cours</a></li>
-                    <li><img src="images/prof.png" class="icon" alt="">  <a href="#">Professeur</a></li>
-                    <li><img src="images/espace-etudiant.png" class="icon" alt="">  <a href="#">Espace Etudiant</a></li>
-                    <li><img src="images/ecrire.png" class="icon" alt="">  <a href="#">Ecrire au Prof</a></li>
+                    <li><img src="images/home.png" class="icon" alt=""> <a href="#">Accueil</a></li>
+                    <li><img src="images/cours.png" class="icon" alt=""> <a href="#">Cours</a></li>
+                    <li><img src="images/prof.png" class="icon" alt=""> <a href="#">Professeur</a></li>
+                    <li><img src="images/espace-etudiant.png" class="icon" alt=""> <a href="#">Espace Etudiant</a></li>
+                    <li><img src="images/ecrire.png" class="icon" alt=""> <a href="#" onclick="openForm()">Ecrire au Prof</a></li>
                 </ul>
             </nav>
 
@@ -68,6 +70,106 @@
         </div>
         <div class="div-right"></div>
     </div>
+    <!-- MESSAGE POP UP BOX -->
+    <!-- STYLE -->
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        .openBtn {
+            display: flex;
+            justify-content: left;
+        }
+
+        .openButton {
+            border: none;
+            border-radius: 5px;
+            background-color: #1c87c9;
+            color: white;
+            padding: 14px 20px;
+            cursor: pointer;
+            position: fixed;
+        }
+
+        .loginPopup {
+            position: relative;
+            text-align: center;
+            width: 100%;
+        }
+
+        .formPopup {
+            display: none;
+            position: fixed;
+            left: 45%;
+            top: 5%;
+            transform: translate(-50%, 5%);
+            border: 3px solid #999999;
+            z-index: 9;
+        }
+
+        .formContainer {
+            max-width: 300px;
+            padding: 20px;
+            background-color: #fff;
+        }
+
+        .formContainer input[type=text] {
+            width: 100%;
+            padding: 15px;
+            margin: 5px 0 20px 0;
+            border: none;
+            background: white;
+            border: 1px solid red;
+            border-radius: 5px;
+        }
+
+        .formContainer .btn {
+            padding: 12px 20px;
+            border: none;
+            background-color: #8ebf42;
+            color: #fff;
+            cursor: pointer;
+            width: 100%;
+            margin-bottom: 15px;
+            opacity: 0.8;
+        }
+
+        .formContainer .cancel {
+            background-color: #cc0000;
+        }
+
+        .formContainer .btn:hover,
+        .openButton:hover {
+            opacity: 1;
+        }
+    </style>
+    <!-- FINSTYLE -->
+    <div class="loginPopup">
+        <div class="formPopup" id="popupForm">
+            <form action="/action_page.php" class="formContainer">
+                <h2>*** GOMYCHAT ***</h2>
+                <br>
+                <label for="message">
+                    <strong>Votre Message: </strong>
+                </label>
+                <br>
+                <input type="text" id="message" placeholder="Message..." name="message" required>
+                <button type="submit" name="send" class="btn">Envoyer</button>
+                <button type="button" class="btn cancel" onclick="closeForm()">Fermer</button>
+            </form>
+        </div>
+    </div>
+    <script>
+        function openForm() {
+            document.getElementById("popupForm").style.display = "block";
+        }
+
+        function closeForm() {
+            document.getElementById("popupForm").style.display = "none";
+        }
+    </script>
 
 </body>
+
 </html>
