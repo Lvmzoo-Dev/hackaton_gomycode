2f969500818dec7a7a87ac6d215710a1cf81a104
Junior Diedhiou <76671685+Juniorito90@users.noreply.github.com>
2022-10-16 02:22:46 +0000

    Modification du dashboard de l'etudiant

--- dashboardEtu.php
+++ dashboardEtu.php
@@ -9,6 +9,11 @@
 </head>
 <body>
     
+    <?php
+
+    // @include 'nav.php';
+
+    ?>
     <div class="container">
         <div class="div-left">
             <div class="profil">
@@ -27,11 +32,40 @@
                     <li><img src="images/cours.png" class="icon" alt="">  <a href="#">Cours</a></li>
                     <li><img src="images/prof.png" class="icon" alt="">  <a href="#">Professeur</a></li>
                     <li><img src="images/espace-etudiant.png" class="icon" alt="">  <a href="#">Espace Etudiant</a></li>
+                    <li><img src="images/ecrire.png" class="icon" alt="">  <a href="#">Ecrire au Prof</a></li>
                 </ul>
             </nav>
 
         </div>
-        <div class="div-middle"></div>
+        <div class="div-middle">
+            <h2>Formation: <span></span></h2>
+            <table border="1">
+                <tr>
+                    <th>Heures Totales</th>
+                    <th>Suivies</th>
+                    <th>Restantes</th>
+                    <th>Niveau</th>
+                </tr>
+                <tr>
+                    <td><?php  ?></td>
+                    <td><?php  ?></td>
+                    <td><?php  ?></td>
+                    <td><?php  ?></td>
+                </tr>
+            </table>
+            <h2 style="color: #BC0000;">COURS</h2>
+            <div class="chapitre">
+                <div class="headChapitre">
+                    <p class="title">CHAPITRE 5</p>
+                    <span class="statutChapitre">Terminée</span>
+                </div>
+                <p class="detailChapitre">
+                    Lorem ipsum dolor sit amet consectetur <br>
+                    adipisicing elit. Maxime, iusto pariatur
+                </p>
+                <a href="#" class="details">Details</a>
+            </div>
+        </div>
         <div class="div-right"></div>
     </div>
 
